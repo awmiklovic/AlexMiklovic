a1bc11adc66b02add5e031d77252f201d35eb6c8
Alex Miklovic <awmiklovic@gmail.com>
2018-11-06 20:40:27 -0800

    Updates

--- src/js/main.js
+++ src/js/main.js
@@ -55,7 +55,7 @@ $(document).ready(function(){
 		fadeOut: function() {
 
 			return $(this.oldContainer).css('opacity', '0').delay(400).promise();
-		 
+
 		},
 
 		fadeIn: function() {
@@ -76,7 +76,8 @@ $(document).ready(function(){
 				$el.css('transition', '600ms');
 				$el.css('opacity', '1');
 				_this.done();
-			}, 100);
+			}, 200);
+
 		}
 
 	});
@@ -100,9 +101,9 @@ $(document).ready(function(){
 	    /**
 	     * this.oldContainer is the HTMLElement of the old Container
 	     */
-	     
-	     return $(this.oldContainer).css('opacity', '0').delay(2000).promise();
-	     
+
+	     return $(this.oldContainer).css('opacity', '0').delay(800).promise();
+
 	  },
 
 	  fadeIn: function() {
@@ -191,6 +192,19 @@ $(document).ready(function(){
 		flipping.flip();
 	}
 
+	const hidePreloader = () => {
+		flipping.read();
+		$('#name').css('display', 'block');
+		$('#preloader').css('display', 'none');
+		flipping.flip();
+		setTimeout(function(){
+			$('#bio .vaporize').removeClass('hide');
+		}, 300);
+		setTimeout(function(){
+			$('.portfolio').removeClass('hidden');
+		}, 600);
+	}
+
 	//Bind Portfolio Images
 
 	$('.portfolio a:not(.no-trigger)').click(function(){
@@ -204,12 +218,12 @@ $(document).ready(function(){
 				$(el).addClass('hidden');
 			}, delay);
 		});
-		setTimeout(function(){
-			hideBlocks();
-		}, 1300);
+		//setTimeout(function(){
+			//hideBlocks();
+		//}, 1300);
 		setTimeout(function(){
 			showNav();
-		}, 2000);
+		}, 1000);
 	});
 
 	$('#nav-name').click(function(){
@@ -245,15 +259,34 @@ $(document).ready(function(){
 
 		const animateArray = Array.from($('.vaporize span'));
 		animateArray.forEach(function(element){
-		  const transition = 1500;
+		  const transition = 3000;
 		  const delayRange = 600;
 		  const rand = Math.floor(Math.random() * delayRange);
 		  $(element).css('transition', transition + 'ms');
 		  $(element).css('transition-delay', rand + 'ms');
 		  $(element).css('transition-property', 'opacity');
+			//$(element).css('transition-timing-function', 'ease-in');
 		});
 
 	});
+
+//Preloader Loading
+		let delay = 0;
+		let interval = 150;
+		const numChars = 12;
+		$('#preload-inner img').each(function(){
+			delay += interval;
+			const el = $(this);
+			setTimeout(function(){
+				$(el).removeClass('hidden');
+			}, delay);
+		});
+		setTimeout(function(){
+			//hidePreloader();
+			$('#preload-inner img').mouseover(function(){
+				hidePreloader();
+			});
+		}, interval * (numChars+1));
 
 
 	//Lazy Load Videos
@@ -292,8 +325,8 @@ $(document).ready(function(){
 		clearTimeout(globalTimeout);
 		const inner = $(this).children('.inner');
 		const video = inner.children('video');
-		const overlay = inner.children('.vid-overlay');	
-		const wireframe = $(this);	
+		const overlay = inner.children('.vid-overlay');
+		const wireframe = $(this);
 		if(video.length > 0){
 			wireframe.removeClass('show');
 			globalTimeout = setTimeout(function(){
@@ -303,7 +336,149 @@ $(document).ready(function(){
 		}
 	});
 
+//Preloader Stuff
+/*
+	var renderer, scene, camera, light, circles;
+
+	var ww      = window.innerWidth,
+	wh          = window.innerHeight,
+	speed       = .2,
+	mouseX       = 0,
+	colors      = [
+	                0xFFFFFF,0xFFFFFF,0xFFFFFF,0xFFFFFF,
+									0xFFFFFF,0xFFFFFF,0xFFFFFF,0xFFFFFF,
+									0xFFFFFF,0xFFFFFF,0xFFFFFF,0xFFFFFF
+	            ],
+	closest     = {position:{z:0}},
+	farest      = {position:{z:0}},
+	radius      = 2,
+	segments    = 4;
+	function init(){
+
+	    renderer = new THREE.WebGLRenderer({canvas : document.getElementById('scene')});
+	    renderer.setSize(ww,wh);
+
+	    scene = new THREE.Scene();
+	    scene.fog = new THREE.Fog( 0xFFFFFF, 300, 700 );
+	    scene.background = new THREE.Color(0xFAFAFA);
+
+	    camera = new THREE.PerspectiveCamera(50,ww/wh, 0.1, 10000 );
+	    camera.position.set(0,0,0);
+	    scene.add(camera);
+
+	    window.addEventListener("mousemove", mousemove);
+	    window.addEventListener("resize", resize);
+
+	    createCircles();
+
+			//$('#preload-inner img').click(function(){
+				//scene.background = new THREE.Color(0xFFFFFF);
+			//});
+
+	}
+
+	var resize = function(){
+	    ww = window.innerWidth;
+	    wh = window.innerHeight;
+	    camera.aspect = ww / wh;
+	    camera.updateProjectionMatrix();
+
+	    renderer.setSize( ww, wh );
+	};
+	var mousemove = function(e){
+	    //speed = (wh/2-e.clientY)/(wh/2)*1;
+	    mouseX = (ww/2-e.clientX)/(ww/5)*5;
+	};
+
+	var createCircles = function(){
+
+	    circles = new THREE.Object3D();
+	    scene.add(circles);
+
+	    for(var i=0;i<20;i++){
+	        addCircle();
+	    }
+	    render();
+
+	};
+
+	var removeLine = function(isFarest){
+	    if(isFarest){
+	       for(var i=0,j=circles.children.length;i<j;i++){
+	            if(circles.children[i] === farest){
+	                circles.remove(circles.children[i]);
+	            }
+	        }
+	    }
+	    else{
+	        for(var i=0,j=circles.children.length;i<j;i++){
+	            if(circles.children[i] === closest){
+	                circles.remove(circles.children[i]);
+	            }
+	        }
+	    }
+	};
+
+
+	var addCircle = function(top){
+	    var row = new THREE.Object3D();
+	    if(top){
+	        row.degreesRotation = (closest.degreesRotation-1) || 0;
+	    }
+	    else{
+	        row.degreesRotation = (farest.degreesRotation+1) || 0;
+	    }
+	    for(var j=0;j<12;j++){
+	        var material = new THREE.MeshBasicMaterial({
+	            color: colors[j]
+	        });
+	        var circleGeometry = new THREE.CircleGeometry( radius, segments );
+	        var circle = new THREE.Mesh( circleGeometry, material );
+	        var translate = new THREE.Matrix4().makeTranslation(30,0,0);
+	        var rotation =  new THREE.Matrix4().makeRotationZ(Math.PI*2/12*j+row.degreesRotation*.3);
+	        circle.applyMatrix( new THREE.Matrix4().multiplyMatrices(rotation, translate) );
+	        row.add(circle);
+	    }
+	    if(top){
+	        row.position.z = (closest.position.z/35+1)*35;
+	    }
+	    else{
+	        row.position.z = (farest.position.z/35-1)*35;
+	    }
+	    circles.add(row);
+	    closest = circles.children[0];
+	    farest = circles.children[0];
+	    for(var i=0,j=circles.children.length;i<j;i++){
+	        if(circles.children[i].position.z>closest.position.z){
+	            closest = circles.children[i];
+	        }
+	        if(circles.children[i].position.z<farest.position.z){
+	            farest = circles.children[i];
+	        }
+	    }
+	};
+
+
+	var render = function () {
+	    requestAnimationFrame(render);
+
+	    camera.position.z -= speed;
+	    camera.position.x += (mouseX-camera.position.x)*.008;
+	    // If closest element is behind camera
+	    if(camera.position.z<(closest.position.z-35) && speed>0){
+	        removeLine(false);
+	        addCircle();
+	    }
+	    else if(camera.position.z>(farest.position.z+665) && speed<0){
+	        removeLine(true);
+	        addCircle(true);
+	    }
+
+	    renderer.render(scene, camera);
+
+	};
+
+	init();
+	*/
+
 });
-
-
-
